153e510110248ef85904afed10d66ec5cc1311fb
rafayatrasel <mdraselravi18@gmail.com>
2017-06-07 11:18:35 +0600

    47

---
+++ 47-testmonial-carousel/assets/js/main.js
@@ -0,0 +1,31 @@
+
+// Class 
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        
+        $(".testimonial-carousel").owlCarousel({
+			items: 1,
+			loops: true,
+			autoplay : false,
+			nav: false,
+			dots: true,
+		});
+        
+        
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
