030718752b4cef6c0ea8f0cbc5ea4c5b5945e7ac
rafayatrasel <mdraselravi18@gmail.com>
2017-06-08 17:44:25 +0600

    50-owl-carousel

---
+++ 50-owl-carousel/assets/js/main.js
@@ -0,0 +1,32 @@
+
+// Class 
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        
+        $(".listing-carousel").owlCarousel({
+			items: 3,
+			loop: true,
+			dots: true,
+			autoplay: false,
+			nav: false,
+			margin:20
+		});
+        
+        
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
